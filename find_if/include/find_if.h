bdc8bbeb598a62bee7b11fdaa244000b152bd2d6
Pablo Santos <pablosantos.if@gmail.com>
2021-06-27 17:22:39 -0300

    Função find_if feita

--- find_if/include/find_if.h
+++ find_if/include/find_if.h
@@ -10,14 +10,28 @@ using std::sort;
 
 namespace graal {
 
-/*! 
- * TODO: documentação no estilo doxygen
+/*!
+ * 
+ *@tparam InputIt iterator para o range.
+ *@tparam UnaryPredicate Predicado para a verificação
+ *
+ * @param first Ponteiro para o primeiro elemento do range.
+ * @param last Ponteiro para a posição logo após o último elemento do range.
+ * @param p uma função que retorna true quando uma determinada condição é satisfeita ou falso caso contrário.
+ * 
+ * @return Um iterator apontando para o primeiro elemento do range que satisfaz p.
  */
 template<class InputIt, class UnaryPredicate>
 InputIt find_if(InputIt first, InputIt last, UnaryPredicate p)
 {
-    // TODO
-    return first;
+    while(first!=last){
+        if(p(*first)){
+            return first;
+        }
+        first++;
+    }
+
+    return last;
 }
 
 }
