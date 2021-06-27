a7a551c4029ce772963ab8f0ef00f27b87c1fd00
Pablo Santos <pablosantos.if@gmail.com>
2021-06-27 17:59:13 -0300

    Função all_any_none feita
    Apenas 11 dos 12 testes foram satisfeitos.

--- all_any_none_of/include/all_any_none_of.h
+++ all_any_none_of/include/all_any_none_of.h
@@ -10,34 +10,76 @@ using std::sort;
 
 namespace graal {
 
-/*! 
- * TODO: documentação no estilo doxygen
+ /*!
+ * 
+ *@tparam InputIt iterator para o range.
+ *@tparam UnaryPredicate Predicado para a verificação
+ *
+ * @param first Ponteiro para o primeiro elemento do range.
+ * @param last Ponteiro para a posição logo após o último elemento do range.
+ * @param cmp uma função que retorna true se o elemento é menor do que o segundo, ou falso caso contrário.
+ * @param p uma função que retorna true quando uma determinada condição é satisfeita ou falso caso contrário.
+ * 
+ * @return  retorna verdadeiro se todos os elementos do range satisfazem p, falso caso contrário
  */
 template<class InputIt, class UnaryPredicate>
 bool all_of(InputIt first, InputIt last, UnaryPredicate p)
 {
-    // TODO
-    return false;
+    while(first!=last){
+        if(!p(*first)){
+            return false;
+        }
+        first++;
+    }
+    return true;
 }
 
-/*! 
- * TODO: documentação no estilo doxygen
+/*!
+ * 
+ *@tparam InputIt iterator para o range.
+ *@tparam UnaryPredicate Predicado para a verificação
+ *
+ * @param first Ponteiro para o primeiro elemento do range.
+ * @param last Ponteiro para a posição logo após o último elemento do range.
+ * @param cmp uma função que retorna true se o elemento é menor do que o segundo, ou falso caso contrário.
+ * @param p uma função que retorna true quando uma determinada condição é satisfeita ou falso caso contrário.
+ * 
+ * @return  retorna verdadeiro se ao menos um elemento do range satisfaz p, falso caso contrário
  */
 template<class InputIt, class UnaryPredicate>
 bool any_of(InputIt first, InputIt last, UnaryPredicate p)
 {
-    // TODO
+    while(first!=last){
+        if(p(*first)){
+            return true;
+        }
+        first++;
+    }
     return false;
 }
 
-/*! 
- * TODO: documentação no estilo doxygen
+/*!
+ * 
+ *@tparam InputIt iterator para o range.
+ *@tparam UnaryPredicate Predicado para a verificação
+ *
+ * @param first Ponteiro para o primeiro elemento do range.
+ * @param last Ponteiro para a posição logo após o último elemento do range.
+ * @param cmp uma função que retorna true se o elemento é menor do que o segundo, ou falso caso contrário.
+ * @param p uma função que retorna true quando uma determinada condição é satisfeita ou falso caso contrário.
+ * 
+ * @return  retorna verdadeiro se todos os elementos do range não satisfazem p, falso caso contrário
  */
 template<class InputIt, class UnaryPredicate>
 bool none_of(InputIt first, InputIt last, UnaryPredicate p)
 {
-    // TODO
-    return false;
+    while(first!=last){
+        if(p(*first)){
+            return false;
+        }
+        first++;
+    }
+    return true;
 }
 
 }
